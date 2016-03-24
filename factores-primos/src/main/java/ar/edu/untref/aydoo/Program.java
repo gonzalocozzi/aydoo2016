49e77bfac6411d66e88c5f2335d2d56520373b70
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-03-24 20:17:28 -0300

    Modificada estructura del progrma, separando comunicacion de logica

--- factores-primos/src/main/java/ar/edu/untref/aydoo/Program.java
+++ factores-primos/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -2,50 +2,25 @@ package ar.edu.untref.aydoo;
 
 import java.io.IOException;
 
-public class Program 
-{
-	//Numero a factorizar
-	private Integer numero;
-
-	/**
-	 * @post se ingresa un numero a factorizar en sus primos
-	 * @param numero_ingresado es el valor a factorizar en sus primos
-	 */
-	public Program(Integer numero_ingresado){
-		this.numero = numero_ingresado;
-	}
-
-	/**
-	 * @post se calculan los factores primos del numero ingresado
-	 * @param numero_factorizado es el valor a factorizar en sus primos
-	 */
-	public void calcular_factores(Integer numero_factorizado){
-		
-		for(int i = 2; i < numero_factorizado; i++){			
-			while(numero%i == 0){				
-				this.numero = numero/i;
-				System.out.print(i + "  ");
-			}
-		}
-
-	}	
+public class Program {
 
 	/**
 	 * Clase principal de ejecucion del programa
+	 * Intermedia entre la clase de factores primos y la terminal
 	 * @param arg
 	 * @throws IOException
 	 */
 	public static void main(String[]arg)throws IOException{
+
+		Integer numero_a_factorizar = Integer.parseInt(arg[0]);
 		
-		Integer num = Integer.parseInt(arg[0]);
-		if(num <= 3){
-			System.out.println("El numero " + num + " no tiene factores primos.");
-		} else {
-			Program factores_primos = new Program(num);
-			System.out.print("Factores primos " + num + ": ");
-			factores_primos.calcular_factores(num);	
-			System.out.print("\n");
-		}		
-	}   
-	
-}
+		FactoresPrimos factores_primos = new FactoresPrimos();
+		factores_primos.setNumero_a_factorizar(numero_a_factorizar);
+		factores_primos.calcular_factores_primos();
+		
+		System.out.print("Factores primos " + numero_a_factorizar + ": ");
+		System.out.print(factores_primos.getFactorizacion());
+		System.out.print("\n");
+	}		
+}   
+
