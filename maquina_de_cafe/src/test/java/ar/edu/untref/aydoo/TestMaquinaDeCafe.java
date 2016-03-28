3349116671c54f0779c2a3ebb930aefc02c96321
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-03-28 11:41:38 -0300

    Prueba exitosa del azucarero agregando azucar al vaso

--- maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/TestMaquinaDeCafe.java
+++ maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/TestMaquinaDeCafe.java
@@ -1,6 +1,18 @@
 package ar.edu.untref.aydoo;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class TestMaquinaDeCafe {
 	
+	@Test
+	public void testAzucareroAgregaNCucharadasDeAzucarAlVaso(){
+		
+		Azucarero azucarero = new Azucarero();
+		Vaso vaso = new Vaso();
+		azucarero.ponerNEnEsteVaso(2, vaso);
+		
+		Assert.assertEquals("EL azucarero agrego con exito azucar al vaso", 2, vaso.getAzucar());
+	}
 
 }
