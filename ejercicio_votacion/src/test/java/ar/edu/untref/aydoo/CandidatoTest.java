d1e88dd75f9c555b4d29b9b854d133ddd8482f1d
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-04-04 23:57:46 -0300

    Prueba exitosa en la que un candidato se crea correctamente con su nombre

---
+++ ejercicio_votacion/src/test/java/ar/edu/untref/aydoo/CandidatoTest.java
@@ -0,0 +1,17 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CandidatoTest {
+	
+	@Test
+	public void testCandidatoSeCreaConNombreCorrecto(){
+		
+		Partido socialismo = new Partido("partido socialista");		
+		Candidato stolbizer = new Candidato("stolbizer", socialismo);
+		
+		Assert.assertEquals("stolbizer", stolbizer.getNombre());
+	}
+
+}
