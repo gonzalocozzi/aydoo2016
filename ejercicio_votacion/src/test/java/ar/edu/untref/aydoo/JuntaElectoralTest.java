0a77930cc53365407aec2351fcf8e0a6666f51b5
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-04-05 00:16:05 -0300

    Prueba exitosa en la que la junta electoral almacena varios candidatos correctamente

--- ejercicio_votacion/src/test/java/ar/edu/untref/aydoo/JuntaElectoralTest.java
+++ ejercicio_votacion/src/test/java/ar/edu/untref/aydoo/JuntaElectoralTest.java
@@ -17,4 +17,24 @@ public class JuntaElectoralTest {
 		Assert.assertTrue(juntaElectoral.tieneCandidato(massa));
 	}
 
+	@Test
+	public void testJuntaElectoralRecibeVariosCandidatos(){
+
+		JuntaElectoral juntaElectoral = new JuntaElectoral();
+
+		Partido renovador = new Partido("frente renovador");
+		Candidato massa = new Candidato("sergio massa", renovador);
+		juntaElectoral.agregarCandidato(massa);
+
+		Partido fpv = new Partido("frente para la victoria");
+		Candidato scioli = new Candidato("daniel scioli", fpv);
+		juntaElectoral.agregarCandidato(scioli);
+
+		Partido pro = new Partido("pro");
+		Candidato macri = new Candidato("macri", pro);
+		juntaElectoral.agregarCandidato(macri);
+
+		Assert.assertTrue(juntaElectoral.tieneCandidato(scioli));		
+	}
+
 }
