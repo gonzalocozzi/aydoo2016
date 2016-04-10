4975b531768da4fa833e51759168ae222cda04f8
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-04-10 17:03:53 -0300

    Test se impide agregar periodicidad nula a la revista

--- ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Revista.java
+++ ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Revista.java
@@ -1,22 +1,30 @@
 package ar.edu.untref.aydoo;
 
 public class Revista extends Producto {
-	
+
 	private Integer periodicidad;
 
-	public Revista(String nombre, Double precio, Integer periodicidad) {
-		
+	public Revista(String nombre, Double precio, Integer periodicidad) throws Exception {
+
 		super(nombre, precio);
-		this.setPeriodicidad(periodicidad);
+		
+		if(periodicidad > 0){
+
+			this.setPeriodicidad(periodicidad);	
+
+		} else {
+
+			throw new Exception("La periodicidad debe ser mayor o igual a 1");
+		}
 	}
 
 	public void setPeriodicidad(Integer periodicidad) {
-		
+
 		this.periodicidad = periodicidad;		
 	}
 
 	public Integer getPeriodicidad() {
-		
+
 		return this.periodicidad;
 	}
 
--- ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/RevistaTest.java
+++ ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/RevistaTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 public class RevistaTest {
 	
 	@Test
-	public void seAgregaPeriodicidadALaRevista() {
+	public void seAgregaPeriodicidadALaRevista() throws Exception {
 		
-		Revista barcelona = new Revista(null, null, null);
+		Revista barcelona = new Revista(null, null, 2);
 		barcelona.setPeriodicidad(2);
 		
 		Assert.assertEquals(2, barcelona.getPeriodicidad(), 0.0);
 	}
 	
 	@Test
-	public void seAgregaPeriodicidadALaRevistaAlSerCreada() {
+	public void seAgregaPeriodicidadALaRevistaAlSerCreada() throws Exception {
 		
 		Revista barcelona = new Revista(null, null, 2);
 		
@@ -23,19 +23,26 @@ public class RevistaTest {
 	}
 	
 	@Test
-	public void seAgregaNombreALaRevistaAlSerCreada() {
+	public void seAgregaNombreALaRevistaAlSerCreada() throws Exception {
 		
-		Revista barcelona = new Revista("barcelona", null, null);
+		Revista barcelona = new Revista("barcelona", null, 2);
 		
 		Assert.assertEquals("barcelona", barcelona.getNombre());
 	}
 	
 	@Test
-	public void seAgregaPrecioALaRevistaAlSerCreada() {
+	public void seAgregaPrecioALaRevistaAlSerCreada() throws Exception {
 		
-		Revista barcelona = new Revista(null, 20.0, null);
+		Revista barcelona = new Revista(null, 20.0, 2);
 		
 		Assert.assertEquals(20.0, barcelona.getPrecio(), 0.0);
+	}
+	
+	@Test (expected=Exception.class)
+	public void seImpideAgregarPeriodicidadNulaALaRevista() throws Exception {
+		
+		@SuppressWarnings("unused")
+		Revista barcelona = new Revista(null, null, 0);
 	}
 
 }
