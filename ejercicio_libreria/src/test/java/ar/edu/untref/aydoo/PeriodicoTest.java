011727b75e6655da1cfc68b7ccb12a0c1f6cba3d
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-04-10 16:49:55 -0300

    Test se agrega nombre al periodico al ser creado

--- ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/PeriodicoTest.java
+++ ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/PeriodicoTest.java
@@ -21,5 +21,13 @@ public class PeriodicoTest {
 		
 		Assert.assertEquals(2, pagina12.getPeriodicidad(), 0.0);
 	}
+	
+	@Test
+	public void seAgregaNombreAlPeriodicoAlSerCreado() {
+		
+		Periodico pagina12 = new Periodico("pagina 12", null, null);
+		
+		Assert.assertEquals("pagina 12", pagina12.getNombre());
+	}
 
 }
