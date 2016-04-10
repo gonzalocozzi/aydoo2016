68023a227ea34858cc604a3c14cdeb090534d7dc
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-04-10 19:08:23 -0300

    Test se agrega nombre al cliente cuando es creado

--- ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Cliente.java
+++ ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Cliente.java
@@ -1,27 +1,32 @@
 package ar.edu.untref.aydoo;
 
 public class Cliente {
-	
+
 	private String nombre;
 	private String direccion;
 
+	public Cliente(String nombre) {
+
+		this.setNombre(nombre);
+	}
+
 	public void setNombre(String nombre) {
-		
+
 		this.nombre = nombre;
 	}
 
 	public String getNombre() {
-		
+
 		return this.nombre;
 	}
 
 	public void setDireccion(String direccion) {
-		
+
 		this.direccion = direccion;
 	}
 
 	public Object getDireccion() {
-		
+
 		return this.direccion;
 	}
 
--- ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/ClienteTest.java
+++ ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/ClienteTest.java
@@ -4,25 +4,33 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ClienteTest {
-	
+
 	@Test
 	public void seAgregaNombreAlCliente() {
-		
-		Cliente cliente = new Cliente();
-		
+
+		Cliente cliente = new Cliente(null);
+
 		cliente.setNombre("juan perez");
-		
+
 		Assert.assertEquals("juan perez", cliente.getNombre());
 	}
-	
+
 	@Test
 	public void seAgregaDireccionAlCliente(){
-		
-		Cliente cliente = new Cliente();
-		
+
+		Cliente cliente = new Cliente(null);
+
 		cliente.setDireccion("Montes de Oca 4720, CABA");
-		
+
 		Assert.assertEquals("Montes de Oca 4720, CABA", cliente.getDireccion());
+	}
+
+	@Test
+	public void seAgregaNombreAlClienteCuandoEsCreado(){
+
+		Cliente cliente = new Cliente("juan perez");
+
+		Assert.assertEquals("juan perez", cliente.getNombre());
 	}
 
 }
