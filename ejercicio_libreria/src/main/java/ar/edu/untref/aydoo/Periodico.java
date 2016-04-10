a6aa4e754347632462d324fdddbe24fcc82c571e
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-04-10 16:58:39 -0300

    Test se impide agregar periodicidad nula al periodico

--- ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Periodico.java
+++ ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Periodico.java
@@ -4,10 +4,18 @@ public class Periodico extends Producto {
 
 	private Integer periodicidad;
 	
-	public Periodico(String nombre, Double precio, Integer periodicidad) {
+	public Periodico(String nombre, Double precio, Integer periodicidad) throws Exception {
 		
 		super(nombre, precio);
-		this.setPeriodicidad(periodicidad);
+		
+		if(periodicidad > 0){
+			
+			this.setPeriodicidad(periodicidad);	
+			
+		} else {
+			
+			throw new Exception("La periodicidad debe ser mayor o igual a 1");
+		}
 	}
 
 	public void setPeriodicidad(Integer periodicidad) {
--- ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/PeriodicoTest.java
+++ ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/PeriodicoTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 public class PeriodicoTest {
 	
 	@Test
-	public void seAgregaPeriodicidadAlPeriodico() {
+	public void seAgregaPeriodicidadAlPeriodico() throws Exception {
 		
-		Periodico pagina12 = new Periodico(null, null, null);
+		Periodico pagina12 = new Periodico(null, null, 2);
 		pagina12.setPeriodicidad(2);
 		
 		Assert.assertEquals(2, pagina12.getPeriodicidad(), 0.0);
 	}
 	
 	@Test
-	public void seAgregaPeriodicidadAlPeriodicoAlSerCreado() {
+	public void seAgregaPeriodicidadAlPeriodicoAlSerCreado() throws Exception {
 		
 		Periodico pagina12 = new Periodico(null, null, 2);
 		
@@ -23,19 +23,26 @@ public class PeriodicoTest {
 	}
 	
 	@Test
-	public void seAgregaNombreAlPeriodicoAlSerCreado() {
+	public void seAgregaNombreAlPeriodicoAlSerCreado() throws Exception {
 		
-		Periodico pagina12 = new Periodico("pagina 12", null, null);
+		Periodico pagina12 = new Periodico("pagina 12", null, 2);
 		
 		Assert.assertEquals("pagina 12", pagina12.getNombre());
 	}
 	
 	@Test
-	public void seAgregaPrecioAlPeriodicoAlSerCreado() {
+	public void seAgregaPrecioAlPeriodicoAlSerCreado() throws Exception {
 		
-		Periodico pagina12 = new Periodico(null, 20.0, null);
+		Periodico pagina12 = new Periodico(null, 20.0, 2);
 		
 		Assert.assertEquals(20.0, pagina12.getPrecio(), 0.0);
+	}
+	
+	@Test (expected=Exception.class)
+	public void seImpideAgregarPeriodicidadNulaAlPeriodico() throws Exception {
+		
+		@SuppressWarnings("unused")
+		Periodico pagina12 = new Periodico(null, null, 0);
 	}
 
 }
