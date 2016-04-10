52901a5c8d4a80e39087924693410e5d7f7b6805
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-04-10 19:03:05 -0300

    Test se agrega direccion al cliente

--- ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Cliente.java
+++ ejercicio_libreria/src/main/java/ar/edu/untref/aydoo/Cliente.java
@@ -3,15 +3,26 @@ package ar.edu.untref.aydoo;
 public class Cliente {
 	
 	private String nombre;
+	private String direccion;
 
 	public void setNombre(String nombre) {
 		
 		this.nombre = nombre;
 	}
 
-	public Object getNombre() {
+	public String getNombre() {
 		
 		return this.nombre;
+	}
+
+	public void setDireccion(String direccion) {
+		
+		this.direccion = direccion;
+	}
+
+	public Object getDireccion() {
+		
+		return this.direccion;
 	}
 
 }
--- ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/ClienteTest.java
+++ ejercicio_libreria/src/test/java/ar/edu/untref/aydoo/ClienteTest.java
@@ -6,13 +6,23 @@ import org.junit.Test;
 public class ClienteTest {
 	
 	@Test
-	public void seAgregaNombreACliente() {
+	public void seAgregaNombreAlCliente() {
 		
 		Cliente cliente = new Cliente();
 		
 		cliente.setNombre("juan perez");
 		
 		Assert.assertEquals("juan perez", cliente.getNombre());
+	}
+	
+	@Test
+	public void seAgregaDireccionAlCliente(){
+		
+		Cliente cliente = new Cliente();
+		
+		cliente.setDireccion("Montes de Oca 4720, CABA");
+		
+		Assert.assertEquals("Montes de Oca 4720, CABA", cliente.getDireccion());
 	}
 
 }
