7aab4686bc51ab088bb6e063b3f0edc44b8552ed
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-04-20 15:33:37 -0300

    Test program devuelve la factorizacion de un numero sin ningun argumento de formato o configuracion

---
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/ProgramTest.java
@@ -0,0 +1,35 @@
+package ar.edu.untref.aydoo;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProgramTest {
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();	
+
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	}
+	
+	@Test
+	public void programDevuelveFactorizacionDeUnNumeroSinArgumentosAparteDelNumero() throws IOException{
+		
+		String[] args = {"82"};
+		Program.main(args);
+		
+		Assert.assertEquals("Factores primos 82: 2 41 \n", outContent.toString());
+	}
+
+}
