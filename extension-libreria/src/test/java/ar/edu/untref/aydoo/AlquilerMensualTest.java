aa1b4dcb55279e1533ad34ed5b1ca1549ca982ee
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-04-25 11:07:49 -0300

    Test cliente alquila por tres meses un libro y se devuelve correctamente el precio

--- extension-libreria/src/test/java/ar/edu/untref/aydoo/AlquilerMensualTest.java
+++ extension-libreria/src/test/java/ar/edu/untref/aydoo/AlquilerMensualTest.java
@@ -27,7 +27,7 @@ public class AlquilerMensualTest {
 	@Test
 	public void alquilerMensualRegistraLibroAlquilado(){
 		
-		AlquilerMensual alquilerDelHobbit = new AlquilerMensual("Alquiler por 7 dias de El Hobbit", 0);
+		AlquilerMensual alquilerDelHobbit = new AlquilerMensual("Alquiler de El Hobbit", 0);
 		alquilerDelHobbit.setLibroAlquilado(elHobbit);
 		
 		Assert.assertEquals("El Hobbit", alquilerDelHobbit.getLibroAlquilado().getNombre());
@@ -36,7 +36,7 @@ public class AlquilerMensualTest {
 	@Test
 	public void alquilerMensualRegistraMesesDelAlquiler(){
 		
-		AlquilerMensual alquilerDelHobbit = new AlquilerMensual("Alquiler por 7 dias de El Hobbit", 0);
+		AlquilerMensual alquilerDelHobbit = new AlquilerMensual("Alquiler por un mes de El Hobbit", 0);
 		alquilerDelHobbit.setMesesDelAlquiler(1);
 		
 		Assert.assertEquals(1, alquilerDelHobbit.getMesesDelAlquiler());
@@ -45,13 +45,25 @@ public class AlquilerMensualTest {
 	@Test
 	public void clienteAlquilaUnEjemplarDelHobbitPorUnMes() throws UnregisteredClientException, RepeatedMonthException, InvalidRentalException{		
 
-		AlquilerMensual alquilerDelHobbit = new AlquilerMensual("Alquiler por 7 dias de El Hobbit", 0);
+		AlquilerMensual alquilerDelHobbit = new AlquilerMensual("Alquiler por un mes de El Hobbit", 0);
 		alquilerDelHobbit.setMesesDelAlquiler(1);
 		alquilerDelHobbit.setLibroAlquilado(elHobbit);
 		
 		maria.comprar(miLibreria, Month.DECEMBER, alquilerDelHobbit);
 
 		Assert.assertEquals(200.0, miLibreria.calcularMontoACobrar(Month.DECEMBER, maria), 0.1);
+	}
+	
+	@Test
+	public void clienteAlquilaUnEjemplarDelHobbitPorTresMeses() throws UnregisteredClientException, RepeatedMonthException, InvalidRentalException{		
+
+		AlquilerMensual alquilerDelHobbit = new AlquilerMensual("Alquiler por 3 meses de El Hobbit", 0);
+		alquilerDelHobbit.setMesesDelAlquiler(3);
+		alquilerDelHobbit.setLibroAlquilado(elHobbit);
+		
+		maria.comprar(miLibreria, Month.DECEMBER, alquilerDelHobbit);
+
+		Assert.assertEquals(600.0, miLibreria.calcularMontoACobrar(Month.DECEMBER, maria), 0.1);
 	}
 
 }
